543a57ba2ed8815fef953ad13b84e2494872b598
fschrader1992 <fschrader1992@gmail.com>
2024-01-15 18:41:52 +0100

    [subject] Allow Save/Load to YAML File

--- pyiris/subject.py
+++ pyiris/subject.py
@@ -6,7 +6,9 @@ This class contains all properties/methods related to stimuli.
 import os
 import uuid
 import json
-import codecs
+import ruamel.yaml
+
+from .functions import dump_file
 
 
 class Subject:
@@ -25,39 +27,44 @@ class Subject:
         self.colorspaces = []
         self.data = {}
 
-    def save_to_file(self, path=None, directory=None):
+    def save_to_file(self, path=None, directory=None, filetype="yaml"):
         """
         Save object data to file.
         :param path: location of file.
         :param directory: directory, if file name should be filled automatically.
         """
+
+        if path is not None and "." in path:
+            filetype = path.split(".")[-1]
+
         dt = {}
         dt.update(vars(self))
         dt["uuid"] = str(self.uuid)
 
         if not path:
             path_var = self.short if len(self.short) > 0 else self.uuid
-            path = "subject_{}.json".format(path_var)
+            path = "subject_{}.yaml".format(path_var)
         if directory:
             path = os.path.join(directory, path)
-        save_dir, save_file = os.path.split(path)
-        if save_dir and not os.path.isdir(save_dir):
-            os.mkdir(save_dir)
-        if ".json" not in save_file:
-            path = path + ".json"
-        json.dump(dt, codecs.open(path, 'w', encoding='utf-8'),
-                  separators=(',', ':'), sort_keys=True, indent=4)
+        dump_file(dt, path, filetype)
 
         print("Successfully saved subject data to file {}".format(path))
 
-    def load_from_file(self, path=None):
+    def load_from_file(self, path=None, filetype="yaml"):
         """
         Load from file.
         :param path: location of file.
         """
 
+        if "." in path:
+            filetype = path.split(".")[-1]
+
         with open(path, "r") as f:
-            d = json.load(f)
+            if filetype == "yaml":
+                d = ruamel.yaml.YAML().load(f)
+            else:
+                d = json.load(f)
+
         for a, b in d.items():
             setattr(self, a, self.__class__(b) if isinstance(b, dict) else b)
 
